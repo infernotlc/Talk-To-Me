85d6689a16e944884be239bf073cc98a00880352
admcilgin <admcilgin@gmail.com>
2023-07-27 15:14:26 +0300

    fixed unknown

--- app/src/main/java/com/example/talk_to_me/helper/SharedPrefsHelper.kt
+++ app/src/main/java/com/example/talk_to_me/helper/SharedPrefsHelper.kt
@@ -22,7 +22,7 @@ object SharedPrefsHelper {
     const val SESSION_TOKEN = "SESSION_TOKEN"
     const val NOTIFICATION_STATE = "NOTIFICATION_STATE"
     const val TCP_CONNECTION = "TCP_CONNECTION"
-    const val CALL_TYPE = "CALL_TYPE"
+    const val CALL_TYPE = "CALL_TYPEM"
 
 
 
